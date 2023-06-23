aa70cfc6348bb278234c10799cda93180bfcbabc
thisistanzeela <86124816+thisistanzeela@users.noreply.github.com>
2023-06-23 19:52:58 +0530

    Update Login.js

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,38 +2,50 @@ import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 function Login({ email, password, setLoggedIn, setNavigate }) {
+  const [emailVal, setEmailVal] = useState('');
+  const [passwordVal, setPasswordVal] = useState('');
 
-    const [emailval, setemailval] = useState("");
-    const [passval, setpassval] = useState("");
-    const handleSubmit = (e) => {
-        e.preventDefault();
+  const handleSubmit = (e) => {
+    e.preventDefault();
 
-        if (emailval == email && passval == password && emailval) {
-            setLoggedIn(true)
-        }
-
+    if (emailVal === email && passwordVal === password && emailVal) {
+      setLoggedIn(true);
     }
-    return (
-        <div id='loginPage'>
-            <h1 id='login-tag'>Login</h1>
+  };
 
-            <form onSubmit={handleSubmit}>
+  return (
+    <div id='loginPage'>
+      <h1 id='login-tag'>Login</h1>
 
-                <h3>Email</h3>
-                <input placeholder='abc@gmail.com' type='email' id='login-email'
-                    value={emailval} onChange={(e) => setemailval(e.target.value)} />
+      <form onSubmit={handleSubmit}>
+        <h3>Email</h3>
+        <input
+          placeholder='abc@gmail.com'
+          type='email'
+          id='login-email'
+          value={emailVal}
+          onChange={(e) => setEmailVal(e.target.value)}
+        />
 
-                <h3>Password</h3>
-                <input placeholder='Enter password' type='password' id='login-password'
-                    value={passval} onChange={(e) => setpassval(e.target.value)} /><br />
-                <button id='login-submit' type='submit'>Login</button>
-            </form>
-            <br />
-            Don't have an account ?
-            <button id='login-to-register' onClick={() => setNavigate(false)}><Link to="/Register">Register here ..</Link></button>
+        <h3>Password</h3>
+        <input
+          placeholder='Enter password'
+          type='password'
+          id='login-password'
+          value={passwordVal}
+          onChange={(e) => setPasswordVal(e.target.value)}
+        /><br />
 
+        <button id='login-submit' type='submit'>Login</button>
+      </form>
 
-        </div>
-    )
+      <br />
+      Don't have an account?
+      <button id='login-to-register' onClick={() => setNavigate(false)}>
+        <Link to="/register">Register here..</Link>
+      </button>
+    </div>
+  );
 }
+
 export default Login;
