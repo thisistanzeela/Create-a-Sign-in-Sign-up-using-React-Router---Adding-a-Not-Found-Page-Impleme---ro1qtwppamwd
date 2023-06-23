ad1cead8b6716ea3c823a274a3e7f44097e4d7af
thisistanzeela <86124816+thisistanzeela@users.noreply.github.com>
2023-06-23 19:51:46 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,41 +1,56 @@
-import React,{useState} from 'react'
+import React, { useState } from 'react';
 import '../styles/App.css';
-import { BrowserRouter as Router ,Route,Switch ,Redirect,NavLink,Link} from 'react-router-dom' ;
-import Login from './Login.js'
-import Register from './Register.js'
+import { BrowserRouter as Router, Route, Switch, Redirect, NavLink, Link } from 'react-router-dom';
+import Login from './Login.js';
+import Register from './Register.js';
 import Dashboard from './Dashboard';
 
-
 const App = () => {
+  const [loggedIn, setLoggedIn] = useState(false);
+  const [navigate, setNavigate] = useState(false);
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
-//code for  Route path="/" , Route path="/dashboard" 
-
-  const [loggedIn,setLoggedIn]=useState(false)
- const [navigate,setNavigate]=useState(false)
-  const[email,setEmail]=useState('')
-  const[password,setPassword]=useState('')
   return (
+    <div id='App'>
+      <Router>
+        <Switch>
+          <Route path="/" exact>
+            {loggedIn ? (
+              <Redirect to="/dashboard" />
+            ) : (
+              <Login
+                email={email}
+                password={password}
+                setLoggedIn={setLoggedIn}
+                setNavigate={setNavigate}
+              />
+            )}
+          </Route>
 
-<div id='App'>
-  <Router>
-    
-    <Switch>
-      <Route path="/" exact>
-     <Login email={email} password={password} setLoggedIn={setLoggedIn} setNavigate={setNavigate}/>
-      </Route>
-      
-      <Route path="/Register" exact>
-        {navigate? <Redirect to="/" /> : <Register  emailChange={setEmail}  passwordChange={setPassword} setNavigate={setNavigate} />}
-      </Route>
+          <Route path="/register" exact>
+            {navigate ? (
+              <Redirect to="/" />
+            ) : (
+              <Register
+                emailChange={setEmail}
+                passwordChange={setPassword}
+                setNavigate={setNavigate}
+              />
+            )}
+          </Route>
 
-      <Route path="/dashboard" exact>
-       <Dashboard setLoggedIn={setLoggedIn}/> 
-      </Route>
-    </Switch>
-  </Router>
-</div>
-  )
-}
-
+          <Route path="/dashboard" exact>
+            {!loggedIn ? (
+              <Redirect to="/" />
+            ) : (
+              <Dashboard setLoggedIn={setLoggedIn} />
+            )}
+          </Route>
+        </Switch>
+      </Router>
+    </div>
+  );
+};
 
 export default App;
